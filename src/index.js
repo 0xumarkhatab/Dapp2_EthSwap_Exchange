c7f3feb806b244b3a05cec6f2cd1c50fcab35149
Umar Khatab <umarkhatab467@gmail.com>
2022-03-08 16:25:54 +0000

    Fixed React Loader Import error in App.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from "react-redux";
 import store from "./Reducer"
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import "./dist/loader/css/react-spinner-loader.css";
+
 
 ReactDOM.render(
 
